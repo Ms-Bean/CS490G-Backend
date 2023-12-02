1f5aab69b0abb42bb1a5d30a9f320cbad0881410
Hasan Ozgur <62520352+hberkozgur@users.noreply.github.com>
2023-12-02 14:23:09 -0500

    accept_reject_clients_business_layer function for the backend for the coach dashboard to accept or reject a users request to be a client of theirs.

--- business_layer/coach_dashboard.js
+++ business_layer/coach_dashboard.js
@@ -20,4 +20,23 @@ async function get_coach_dashboard_info(coach_id)
     });
 }
 
+async function accept_reject_clients_business_layer(coach_id, client_id, accept_reject_response) {
+    try {
+        const coachRole = await user_info.get_role_business_layer(coach_id);
+
+        if (coachRole !== "coach") {
+            throw new Error("User is not a coach.");
+        }
+
+        const response = await coach_dashboard.accept_reject_clients_data_layer(coach_id, client_id, accept_reject_response);
+
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+
+module.exports.accept_reject_clients_business_layer = accept_reject_clients_business_layer;
 module.exports.get_coach_dashboard_info = get_coach_dashboard_info;
