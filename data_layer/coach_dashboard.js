8559918aaf0d681b8a72f90a875626b7bcdbcd83
Hasan Ozgur <62520352+hberkozgur@users.noreply.github.com>
2023-12-02 14:15:59 -0500

    data layer accept_reject_clients_data_layer function for accepting or rejecting the accept_reject_clients_data_layer

--- data_layer/coach_dashboard.js
+++ data_layer/coach_dashboard.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 let connection = require("./conn");
 let con = connection.con;
 
@@ -16,4 +17,42 @@ async function get_coach_dashboard_info(coach_id)
     });
 }
 
+async function accept_reject_clients_data_layer(coach_id, client_id, accept_reject_response) {
+    return new Promise((resolve, reject) => {
+        let sql;
+        
+        if (accept_reject_response === "reject") {
+            sql = `DELETE FROM client_coach
+                    WHERE coach_id = ?
+                    AND client_id = ?
+                    AND request = 1;
+                `;
+        } else if (accept_reject_response === "accept") {
+            sql = `UPDATE client_coach
+                    SET request = 0
+                    WHERE coach_id = ?
+                    AND client_id = ?
+                    AND request = 1;        
+                `;
+        } else {
+            reject("Invalid accept_reject_response value");
+            return;
+        }
+
+        con.query(sql, [coach_id, client_id], function(err, result) {
+            if (err) {
+                console.error(err);
+                reject("Something went wrong in our database.");
+            }
+
+            resolve(result);
+        });
+    });
+}
+
+
+
+
+
+module.exports.accept_reject_clients_data_layer = accept_reject_clients_data_layer;
 module.exports.get_coach_dashboard_info = get_coach_dashboard_info;
