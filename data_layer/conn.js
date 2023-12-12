edd050354731a3227a58c3a13407da777ab33fc1
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-12-12 10:22:20 -0500

    Update conn.js

--- data_layer/conn.js
+++ data_layer/conn.js
@@ -5,7 +5,7 @@ const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: database_name
+    database: process.env.DB_NAME,
 });
 
 module.exports.con = con;
